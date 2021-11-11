75daeec282a24d0e90ba19e82820f81e0df20784
Pierre Yager <pierre.yager@crisalid.com>
2021-11-11 19:17:55 +0100

    Added errors, refactored out the voting mechanic into do_vote(), requires a signer to update an item

--- backend/programs/quotes_gif_program/src/lib.rs
+++ backend/programs/quotes_gif_program/src/lib.rs
@@ -2,17 +2,50 @@ use anchor_lang::prelude::*;
 
 declare_id!("4H8RWS5zZsj34WitF4JEuVLUxmsQgYBA9XxWBQBT52Sz");
 
+mod quotes_gif_internals {
+  use super::*;
+
+  pub fn do_vote(base_account: &mut Account<BaseAccount>, index: usize, vote: Vote) -> Result<()> {
+    if index < base_account.gif_list.len() {
+      let mut item = &mut base_account.gif_list[index];
+      item.votes += vote as i64;
+      Ok(())
+    }
+    else {
+      Err(ErrorCode::GifIndexOutOfBounds.into())
+    }
+  }
+}
+
 #[program]
 pub mod quotes_gif_program {
   use super::*;
-  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
-    // Get a reference to the account.
+
+  // initialize the program once
+  pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
     let base_account = &mut ctx.accounts.base_account;
-    // Initialize total_gifs.
     base_account.total_gifs = 0;
     Ok(())
   }
 
+  // // send $SOL to someone else
+  // pub fn send_sol(ctx: Context<SendSol>, amount: u64) -> ProgramResult {
+  //   let ix = anchor_lang::solana_program::system_instruction::transfer(
+  //     &ctx.accounts.from.key(),
+  //     &ctx.accounts.to.key(),
+  //     amount
+  //   );
+
+  //   anchor_lang::solana_program::program::invoke(
+  //     &ix,
+  //     &[
+  //       ctx.accounts.from.to_account_info(),
+  //       ctx.accounts.to.to_account_info()
+  //     ]
+  //   )
+  // }
+
+  // register a new GIF
   pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
     let base_account = &mut ctx.accounts.base_account;
     let user = &mut ctx.accounts.user;
@@ -31,53 +64,41 @@ pub mod quotes_gif_program {
     Ok(())
   }
 
-  pub fn update_item(ctx: Context<UpdateItem>, index: u64, vote: Vote) -> ProgramResult {
+  // Update GIF
+  pub fn update_item(ctx: Context<UpdateItem>, index: u64, vote: Vote) -> Result<()> {
     let base_account = &mut ctx.accounts.base_account;
-
-    let i = index as usize;
-    if i < base_account.gif_list.len() {
-      let mut item = &mut base_account.gif_list[i];
-      item.votes += vote as i64;
-    }
-
-    Ok(())
+    quotes_gif_internals::do_vote(base_account, index as usize, vote)
   }
 
-  pub fn up_vote(ctx: Context<UpdateItem>, index: u64) -> ProgramResult {
+  pub fn up_vote(ctx: Context<UpdateItem>, index: u64) -> Result<()> {
     let base_account = &mut ctx.accounts.base_account;
-
-    let i = index as usize;
-    if i < base_account.gif_list.len() {
-      let mut item = &mut base_account.gif_list[i];
-      item.votes += 1;
-    }
-
-    Ok(())
+    quotes_gif_internals::do_vote(base_account, index as usize, Vote::Up)
   }
 
-  pub fn down_vote(ctx: Context<UpdateItem>, index: u64) -> ProgramResult {
+  pub fn down_vote(ctx: Context<UpdateItem>, index: u64) -> Result<()> {
     let base_account = &mut ctx.accounts.base_account;
-
-    let i = index as usize;
-    if i < base_account.gif_list.len() {
-      let mut item = &mut base_account.gif_list[i];
-      item.votes -= 1;
-    }
-
-    Ok(())
+    quotes_gif_internals::do_vote(base_account, index as usize, Vote::Down)
   }
 }
 
 // Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct StartStuffOff<'info> {
+pub struct Initialize<'info> {
   #[account(init, payer = user, space = 9000)]
   pub base_account: Account<'info, BaseAccount>,
-
   #[account(mut)]
   pub user: Signer<'info>,
   pub system_program: Program <'info, System>,
 }
+
+// #[derive(Accounts)]
+// pub struct SendSol<'info> {
+//   #[account(mut)]
+//   pub from: Signer<'info>,
+//   #[account(mut)]
+//   pub to: AccountInfo<'info>,
+//   pub system_program: Program<'info, System>,
+// }
 
 // Specify what data you want in the AddGif Context.
 // Getting a handle on the flow of things :)?
@@ -107,6 +128,8 @@ pub enum Vote {
 pub struct UpdateItem<'info> {
   #[account(mut)]
   pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>
 }
 
 // Tell Solana what we want to store on this account.
@@ -115,4 +138,10 @@ pub struct BaseAccount {
   pub total_gifs: u64,
   // Attach a Vector of type ItemStruct to the account.
   pub gif_list: Vec<ItemStruct>,
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("No GIF at this index")]
+    GifIndexOutOfBounds,
 }
--- backend/tests/quotes_gif_program.js
+++ backend/tests/quotes_gif_program.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const anchor = require("@project-serum/anchor");
 
 describe("quote-gif-program", () => {
-  const provider = anchor.Provider.env();
+  const provider = anchor.Provider.local();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.QuotesGifProgram;
@@ -10,10 +10,10 @@ describe("quote-gif-program", () => {
 	// Create an account keypair for our program to use.
   const baseAccount = anchor.web3.Keypair.generate();
 
-  it('Is initializes the program', async () => {
+  it('Is initializes the program', async function() {
     console.log("🚀 Starting test...")
 
-    const tx = await program.rpc.startStuffOff({
+    const tx = await program.rpc.initialize({
       accounts: {
         baseAccount: baseAccount.publicKey,
         user: provider.wallet.publicKey,
@@ -28,7 +28,7 @@ describe("quote-gif-program", () => {
     assert.ok(account.totalGifs.eq(new anchor.BN(0)));
   });
 
-  it('Adds a GIF', async () => {
+  it('Adds a GIF', async function() {
     await program.rpc.addGif("insert_a_giphy_link_here", {
       accounts: {
         baseAccount: baseAccount.publicKey,
@@ -44,12 +44,13 @@ describe("quote-gif-program", () => {
     assert.ok(gifs[0].userAddress.equals(provider.wallet.publicKey));
   });
 
-  it('Upvotes a GIF', async() => {
+  it('Upvotes a GIF', async function() {
     // upVote, downVote and updateItem requires an item index (u64)
     // that should be provided as an anchor.BN()
     await program.rpc.upVote(new anchor.BN(0), {
       accounts: {
         baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
       },
     });
 
@@ -58,10 +59,11 @@ describe("quote-gif-program", () => {
     assert.ok(gifs[0].votes.eq(new anchor.BN(1)));
   });
 
-  it('Updates a GIF with Vote.Up', async() => {
+  it('Updates a GIF with Vote.Up', async function() {
     await program.rpc.updateItem(new anchor.BN(0), { up: {} }, {
       accounts: {
         baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
       }
     });
 
@@ -70,10 +72,11 @@ describe("quote-gif-program", () => {
     assert.ok(gifs[0].votes.eq(new anchor.BN(2)));
   });
 
-  it('Updates a GIF with Vote.Down', async() => {
+  it('Updates a GIF with Vote.Down', async function() {
     await program.rpc.updateItem(new anchor.BN(0), { down: {} }, {
       accounts: {
         baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
       }
     });
 
@@ -82,17 +85,38 @@ describe("quote-gif-program", () => {
     assert.ok(gifs[0].votes.eq(new anchor.BN(1)));
   });
 
-  it('Downvotes a GIF', async() => {
+  it('Downvotes a GIF', async function() {
     // upVote, downVote and updateItem requires an item index (u64)
     // that should be provided as an anchor.BN()
     await program.rpc.downVote(new anchor.BN(0), {
       accounts: {
         baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
       },
     });
 
     let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
     let gifs = account.gifList;
     assert.ok(gifs[0].votes.eq(new anchor.BN(0)));
+  });
+
+  it('Cannot update a non existing GIF (Simulated)', async function() {
+    try {
+      // we know it updateItem() will fail and we don't want the stacktrace
+      await program.simulate.updateItem(new anchor.BN(1), { up: {} }, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+      });
+      assert.ok(false);
+    } catch (err) {
+      // We expect to catch an exception here but the exception object is not really helpful
+      assert.ok(true);
+  //   const errMsg = "No GIF at this index";
+  //   assert.equal(err.toString(), errMsg);
+  //   assert.equal(err.msg, errMsg);
+  //   assert.equal(err.code, 300);
+    }
   });
 });
